0e2ddb7ebeefa53cbd98ac96e721bfb754a87b16
Manuel Ohlendorf <manuel.ohlendorf@bdess.com>
2017-01-13 20:11:47 +0100

    Better names

--- ehz-reader/index.js
+++ ehz-reader/index.js
@@ -6,12 +6,12 @@ const name = 'household'
 new Reader({ port: '/dev/ttyUSB0'}).on('data', (msg) => {
     console.log(name, msg.total)
     
-    client.write('login')
+    client.write('power_consumption')
     .tag({
       meter: name  
     })
     .field({
-      meter: name, value: msg.total
+      meter: name, total: msg.total
     })
     .queue()
     client.syncWrite()
